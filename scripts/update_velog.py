bfc30b042dd696c467eda7f48bbb091d0099dd46
bsjin1122 <kitty7579@hanmail.net>
2024-09-29 00:51:23 +0900

    local-remote branch 맞춰주기

--- scripts/update_velog.py
+++ scripts/update_velog.py
@@ -85,5 +85,5 @@ for entry in feed.entries:
         repo.git.commit('-m', commit_message)
 
 # 변경 사항을 깃허브에 푸시
-name_of_remote_branch = 'update-blog-posts-branch'
-repo.git.push('origin', f'HEAD:{name_of_remote_branch}')
+repo.git.pull('origin', 'update-blog-posts-branch')
+repo.git.push()
